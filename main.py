59c29ad8119a5d3c5a5b0b3d99915e944c60ef41
KoraL22 <133631401+KoraL22@users.noreply.github.com>
2024-02-28 12:56:29 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# print('Podaj imie')
+# name = input()
+# print('Siema  ' + name)
+#
+# print('Podaj a')
+# a = int(input())
+# print('Podaj b')
+# b = int(input())
+# sum = int(a + b)
+# print(float(sum))
+#
+# suma = 0
+# zakres = int(input())
+# for i in range(0, zakres):
+#     liczba = int(input())
+#     suma += liczba
+#     print(f"suma: {suma}")
+#
+# print(sum([int(x) for x in (input("podaj liczby oddzielnie:").split())]))
+#
+# print(sum(range(8, 81)))
+
+print(range(80, 8, -2))
